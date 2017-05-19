b7a156c77b6bc078bf649c5790755b238038c471
tompee <tompee@alink-group.co.jp>
2017-05-19 19:38:48 +0900

    Added firebase support

--- app/src/main/java/com/tompee/mottainai/MainActivity.kt
+++ app/src/main/java/com/tompee/mottainai/MainActivity.kt
@@ -5,17 +5,21 @@ import android.os.Bundle
 import android.support.design.widget.TabLayout
 import android.support.v4.view.ViewPager
 import android.view.View
+import com.google.firebase.auth.FirebaseAuth
 import com.tompee.mottainai.controller.adapter.MainPagerAdapter
 import com.tompee.mottainai.controller.base.BaseActivity
 
+
 class MainActivity : BaseActivity() {
+    private var mAuth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState, true)
 
-        /** Check if already logged in */
-        if (true) {
+        mAuth = FirebaseAuth.getInstance()
+        val currentUser = mAuth?.currentUser
+        if (currentUser == null || !currentUser.isEmailVerified) {
             val intent = Intent(this, LoginActivity::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
             startActivity(intent)
--- app/src/main/java/com/tompee/mottainai/controller/fragment/LoginFragment.kt
+++ app/src/main/java/com/tompee/mottainai/controller/fragment/LoginFragment.kt
@@ -1,16 +1,35 @@
 package com.tompee.mottainai.controller.fragment
 
 import android.content.Context
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import com.tompee.mottainai.MainActivity
 import com.tompee.mottainai.R
+import java.util.regex.Pattern
 
-class LoginFragment : Fragment() {
+
+class LoginFragment : Fragment(), View.OnClickListener {
+    private val EMAIL_PATTERN = "[A-Z0-9a-z_%+-]+(\\.[A-Z0-9a-z_%+-]+)*@[A-Za-z0-9]+([.-][A-Za-z0-9]+)*\\.[A-Za-z]{2,}"
+    private val MIN_PASS_CHAR = 6
     private var listener: LoginFragmentListener? = null
+    private var auth: FirebaseAuth? = null
+    private var userView: EditText? = null
+    private var passView: EditText? = null
+    private var userViewLabel: TextView? = null
+    private var passViewLabel: TextView? = null
+    private var userViewUnderline: View? = null
+    private var passViewUnderline: View? = null
 
     companion object {
         const val LOGIN = 0
@@ -26,6 +45,11 @@ class LoginFragment : Fragment() {
         }
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        auth = FirebaseAuth.getInstance()
+    }
+
     override fun onAttach(context: Context?) {
         super.onAttach(context)
         if (context is LoginFragmentListener) {
@@ -36,7 +60,14 @@ class LoginFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view = inflater?.inflate(R.layout.fragment_login, container, false)
-        val switchButton = view?.findViewById(R.id.button_switch) as Button
+        userView = view?.findViewById(R.id.et_user) as EditText
+        passView = view.findViewById(R.id.et_pass) as EditText
+        userViewLabel = view.findViewById(R.id.tv_user_label) as TextView
+        passViewLabel = view.findViewById(R.id.tv_pass_label) as TextView
+        userViewUnderline = view.findViewById(R.id.view_user_underline)
+        passViewUnderline = view.findViewById(R.id.view_pass_underline)
+
+        val switchButton = view.findViewById(R.id.button_switch) as Button
         val type = arguments?.getInt(TYPE_TAG)
         switchButton.setOnClickListener { listener?.onSwitchPage(type!!) }
         if (type == LOGIN) {
@@ -45,6 +76,7 @@ class LoginFragment : Fragment() {
             switchButton.text = getString(R.string.label_login_registered)
         }
         val commandButton = view.findViewById(R.id.button_command) as Button
+        commandButton.setOnClickListener(this)
         if (type == LOGIN) {
             commandButton.text = getString(R.string.label_login_button)
             commandButton.setBackgroundResource(R.drawable.ripple_login)
@@ -53,6 +85,83 @@ class LoginFragment : Fragment() {
             commandButton.setBackgroundResource(R.drawable.ripple_sign_up)
         }
         return view
+    }
+
+    override fun onClick(v: View?) {
+        val type = arguments?.getInt(TYPE_TAG)
+
+        if (!validateEmailField() || !validatePassField()) {
+            return
+        }
+        if (type == SIGN_UP) {
+            auth?.createUserWithEmailAndPassword(userView?.text.toString(),
+                    passView?.text.toString())?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    auth?.currentUser?.sendEmailVerification()
+                } else {
+                    Log.d("hello", "Sign up failed")
+                }
+            }
+        } else {
+            auth?.signInWithEmailAndPassword(userView?.text.toString(),
+                    passView?.text.toString())?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    if (auth?.currentUser?.isEmailVerified!!) {
+                        moveToMainActivity()
+                    } else {
+                        Log.d("hello", "Verify email first")
+                    }
+                } else {
+                    Log.d("hello", "Log in failed")
+                }
+            }
+        }
+    }
+
+    private fun validateEmailField(): Boolean {
+        val email = userView?.text.toString()
+
+        if (email.isEmpty()) {
+            userViewLabel?.setTextColor(Color.RED)
+            userViewUnderline?.setBackgroundColor(Color.RED)
+            return false
+        }
+
+        val ptn = Pattern.compile(EMAIL_PATTERN)
+        val mc = ptn.matcher(email)
+        if (!mc.matches()) {
+            userViewLabel?.setTextColor(Color.RED)
+            userViewUnderline?.setBackgroundColor(Color.RED)
+            return false
+        }
+
+        userViewLabel?.setTextColor(ContextCompat.getColor(context, R.color.colorLight))
+        userViewUnderline?.setBackgroundColor(ContextCompat.getColor(context, R.color.colorLight))
+        return true
+    }
+
+    private fun validatePassField(): Boolean {
+        val pass = passView?.text.toString()
+        if (pass.isEmpty()) {
+            passViewLabel?.setTextColor(Color.RED)
+            passViewUnderline?.setBackgroundColor(Color.RED)
+            return false
+        } else if (pass.length < MIN_PASS_CHAR) {
+            passViewLabel?.setTextColor(Color.RED)
+            passViewUnderline?.setBackgroundColor(Color.RED)
+            return false
+        } else {
+            passViewLabel?.setTextColor(ContextCompat.getColor(context, R.color.colorLight))
+            passViewUnderline?.setBackgroundColor(ContextCompat.getColor(context, R.color.colorLight))
+        }
+        return true
+    }
+
+    private fun moveToMainActivity() {
+        val intent = Intent(context, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+        startActivity(intent)
+        activity.finish()
     }
 
     interface LoginFragmentListener {
